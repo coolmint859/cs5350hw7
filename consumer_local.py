66ddadaed6bcd7e0a51fcf297d4d123c7c0d5f61
Preston Hall <prestonhall@prestonsmacbook-3.local>
2024-11-20 22:31:39 -0700

    added logging support

--- consumer_local.py
+++ consumer_local.py
@@ -7,9 +7,7 @@ import logging
 import glob
 
 
-def create_widget_obj(widget_data):
-    print("creating widget!")
-
+def create_widget_obj(logger, widget_data):
     widget_obj = {"widgetId": widget_data["widgetId"],
                   "owner": widget_data["owner"],
                   "description": widget_data["description"]}
@@ -20,22 +18,18 @@ def create_widget_obj(widget_data):
     if 'label' in widget_data.keys():
         widget_obj["label"] = widget_data["label"]
 
+    logger.info("Created Widget")
     return widget_obj
 
 
-def delete_widget_obj(widget_data):
-    print("delete widget!")
+def delete_widget_obj(logger, widget_data):
+    logger.info("Deleted Widget")
     return widget_data
 
 
-def update_widget_obj(widget_data):
-    print("update widget!")
+def update_widget_obj(logger, widget_data):
+    logger.info("Updated Widget")
     return widget_data
-
-
-def bucketizeName(name):
-    name = name.replace(" ", "-").lower()
-    return name
 
 
 def get_widgets():
@@ -52,30 +46,31 @@ def get_widgets():
     return widgets
 
 
-def push_widget(widget_obj):
-    widget_path = f"sample_responses/{bucketizeName(widget_obj['owner'])}/"
+def push_widget(logger, widget_obj):
+    bucket_owner = widget_obj['owner'].replace(" ", "-").lower()
+    bucket_id = widget_obj['widgetId']
+    widget_path = f"sample_responses/{bucket_owner}/"
 
     if not os.path.exists(widget_path):
         os.makedirs(widget_path)
 
-    with open(widget_path + f"{widget_obj['widgetId']}.json", 'w') as widget_file:
+    with open(widget_path + f"{bucket_id}.json", 'w') as widget_file:
         json.dump(widget_obj, widget_file)
-    print("widget created!")
 
 
-def widget_contains_required_keys(widget_data):
+def widget_contains_required_keys(logger, widget_data):
     with open("widgetRequest-schema.json") as schema_file:
         schema = json.load(schema_file)
 
         for key in schema['required']:
             if key not in widget_data:
+                logger.warning(f"Widget Missing Required Key '{key}', Skipping...")
                 return False
     return True
 
 
-def process_widget(widget):
-    if not widget_contains_required_keys(widget):
-        print(f"{widget['requestId']} does not contain required keys!")
+def process_widget(logger, widget):
+    if not widget_contains_required_keys(logger, widget):
         return
 
     accepted_types = {
@@ -85,22 +80,39 @@ def process_widget(widget):
     }
 
     if widget['type'] in accepted_types:
-        widget_obj = accepted_types[widget['type']](widget)
-        push_widget(widget_obj)
+        widget_obj = accepted_types[widget['type']](logger, widget)
+        push_widget(logger, widget_obj)
     else:
-        print(f"'{widget['type']}' is not a valid widget type!")
+        logger.warning(f"Widget Type '{widget['type']}' is an Invalid Type, Skipping...")
+
+
+def create_logger():
+    logger = logging.getLogger(__name__)
+    logformatter = logging.Formatter("{asctime} {levelname} {name}: {message}", style='{')
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logformatter)
+    logger.addHandler(consoleHandler)
+
+    logging.basicConfig(
+        filename="consumer_logs.txt",
+        filemode='w',
+        format="{asctime} {levelname} {name}: {message}",
+        style="{",
+        datefmt="%H:%M:%S",
+        level=logging.DEBUG)
+
+    return logger
 
 
 def main():
+    logger = create_logger()
+
     max_wait_time = 5  # wait time 5 seconds
     curr_wait_time = 0
 
-
     widgets = get_widgets()
-    # process_widget(widgets[0])
     for widget in widgets:
-        print(widget.keys())
-        process_widget(widget)
+        process_widget(logger, widget)
 
     # while curr_wait_time <= max_wait_time:
     #     widget = get_next_widget()
