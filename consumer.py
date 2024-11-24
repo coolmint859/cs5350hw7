4c38aad6a8ea6a7d70e2c5b0a7808fbdad8c1ba1
Preston Hall <prestonhall@prestonsmacbook-3.local>
2024-11-24 16:39:39 -0700

    added support to save to dynamodb

--- consumer.py
+++ consumer.py
@@ -44,10 +44,12 @@ def get_next_request(logger, s3_client, bucket_name: str) -> (dict[str: str], in
     object_content = response["Body"].read().decode("utf-8")
     widget = json.loads(object_content)
 
+    logger.debug(f"Retrieved request {widget['widgetId']}")
+
     return widget, min_key
 
 
-def save_to_s3(logger, widget_obj, bucket_name) -> None:
+def save_to_s3(logger, widget_obj: dict[str: str], bucket_name: str) -> None:
     s3_client = boto3.client('s3')
 
     bucket_owner = widget_obj['owner'].replace(" ", "-").lower()
@@ -57,11 +59,26 @@ def save_to_s3(logger, widget_obj, bucket_name) -> None:
     widget = json.dumps(widget_obj)
     s3_client.put_object(Bucket=bucket_name, Key=widget_path, Body=widget)
 
-    # logger.info(f"Uploaded widget in {widget_path} as {widget_obj['widgetId']}.json")
+    logger.debug(f"Uploaded widget in {widget_path} as {widget_obj['widgetId']}.json")
 
 
-def save_to_dynamodb(logger, widget_obj, table_name) -> None:
-    pass
+def save_to_dynamodb(logger, widget_obj: dict[str: str], table_name: str) -> None:
+    dynamodb_client = boto3.client("dynamodb")
+
+    item_dict = {"id": {'S': widget_obj["widgetId"]}}
+
+    for attr in widget_obj.keys():
+        if attr == "otherAttributes" or attr == "widgetId":
+            continue
+        item_dict[attr] = {"S": widget_obj[attr]}
+
+    if "otherAttributes" in widget_obj.keys():
+        for table in widget_obj["otherAttributes"]:
+            for attr in table:
+                item_dict[attr] = {"S": table[attr]}
+
+    dynamodb_client.put_item(TableName=table_name, Item=item_dict)
+    logger.debug(f"Uploaded widget in '{table_name}' table as {widget_obj['widgetId']}")
 
 
 def request_contains_required_keys(logger, widget_data: dict[str: str]) -> bool:
@@ -93,7 +110,7 @@ def process_request(logger, request: dict[str: str]) -> dict[str: str]:
         return None
 
 
-def create_logger() -> logging.Logger:
+def create_logger(debug) -> logging.Logger:
     logger = logging.getLogger(__name__)
     logformatter = logging.Formatter("{asctime} {levelname} {name}: {message}", style='{')
     consoleHandler = logging.StreamHandler()
@@ -106,13 +123,13 @@ def create_logger() -> logging.Logger:
         format="{asctime} {levelname} {name}: {message}",
         style="{",
         datefmt="%H:%M:%S",
-        level=logging.INFO)
+        level=logging.DEBUG if debug else logging.INFO)
 
     return logger
 
 
 def main(user_info: dict[str: str]) -> None:
-    logger = create_logger()
+    logger = create_logger(user_info["DEBUG"])
 
     s3 = boto3.client('s3')
     curr_wait_time = 0
@@ -130,8 +147,9 @@ def main(user_info: dict[str: str]) -> None:
             else:
                 save_to_dynamodb(logger, widget, user_info["DYNAMODB_TABLE"])
 
+
             s3.delete_object(Bucket=user_info["REQUEST_BUCKET"], Key=key)
-            # logger.info(f"Fulfilled request '{key}', finding next request...")
+            logger.debug(f"Fulfilled request '{key}', finding next request...\n")
             curr_wait_time = 0
         else:
             time.sleep(0.1)
@@ -146,7 +164,8 @@ def main(user_info: dict[str: str]) -> None:
 @click.option("--widget-bucket", "-wb", help="Name of the s3 bucket that may contain widgets.")
 @click.option("--dynamodb-table", "-dbt", help="Name of the dynamodb table that may contain widgets.")
 @click.option("--max-wait-time", "-mwt", default=3, help="The max wait time in seconds without finding a request")
-def cli(request_bucket, widget_bucket, dynamodb_table, max_wait_time):
+@click.option("--debug/--no-debug", default=False, help="If set, will print information for each step of the process.")
+def cli(request_bucket, widget_bucket, dynamodb_table, max_wait_time, debug):
     if widget_bucket and dynamodb_table:
         click.echo("Error, Mismatched Options. To see possible parameters, type '--help'.")
         return
@@ -163,6 +182,7 @@ def cli(request_bucket, widget_bucket, dynamodb_table, max_wait_time):
         user_info['DYNAMODB_TABLE'] = dynamodb_table
 
     user_info["MAX_WAIT_TIME"] = max_wait_time
+    user_info["DEBUG"] = debug
 
     main(user_info)
 
